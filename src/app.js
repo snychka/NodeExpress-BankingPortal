9c3e7bb29a6c35fd41a506f55c6d721231e999e8
snychka <stefannychka@gmail.com>
2018-07-23 07:35:36 -0600

    module 3

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.set('views', path.join(__dirname, 'views' ));
 app.set('view engine', 'ejs');
 //app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 const accountData = fs.readFileSync('src/json/accounts.json', 'utf8');
 const accounts = JSON.parse(accountData);
@@ -17,6 +18,7 @@ const users = JSON.parse(userData);
 app.get('/', function(req, res) {
   res.render('index', { title: 'Account Summary', accounts: accounts } );
 });
+//app.get('/', (req, res) =>  res.render('index', { title: 'Index' } ));
 app.get('/savings', function(req, res) {
   res.render('account', { account: accounts.savings } );
 });
@@ -29,8 +31,54 @@ app.get('/credit', function(req, res) {
 app.get('/profile', function(req, res) {
   res.render('profile', { user: users[0]} );
 });
+app.get('/transfer', function(req, res) {
+  res.render('transfer');
+});
+app.get('/payment', function(req, res) {
+  res.render('payment');
+});
 
-//app.get('/', (req, res) =>  res.render('index', { title: 'Index' } ));
+// https://stackoverflow.com/questions/5710358/how-to-retrieve-post-query-parameters
+app.post('/transfer', function(req, res) {
+  //console.log(req.body.from);
+  //console.log(req.body.amount);
+
+
+  let from_account_type = req.body.from;
+  let amount = req.body.amount;
+  //console.log('from before: ' + accounts[from_account_type].balance);
+  accounts[from_account_type].balance -= amount;
+  //console.log('from after: ' + accounts[from_account_type].balance);
+
+  let to_account_type = req.body.to;
+  //console.log('to before: ' + accounts[to_account_type].balance);
+  accounts[to_account_type].balance += parseInt(amount, 10);
+  //console.log('to after: ' + accounts[to_account_type].balance);
+
+  let accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(
+      path.join(__dirname, 'json/accounts.json'),
+      accountsJSON,
+      'utf8');
+
+  res.render('transfer', {message: 'Transfer Completed'});
+});
+
+app.post('/payment', function(req, res) {
+
+  let amount = req.body.amount;
+  accounts.credit.balance -= amount;
+  accounts.credit.available += parseInt(amount, 10);
+
+  let accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(
+      path.join(__dirname, 'json/accounts.json'),
+      accountsJSON,
+      'utf8');
+
+  res.render('payment', { message: "Payment Successful"});
+
+});
 
 app.listen(3000, function() {
   console.log('Project Running on port 3000!');
--- test/module3/app-payment-feature.spec.js
+++ test/module3/app-payment-feature.spec.js
@@ -35,6 +35,7 @@ describe('Payment Feature', () => {
     getHandleSpy(getReq, getRes);
 
     assert(getRes.render.called, 'The payment get route may have not been created.');
+    console.log(getRes.render.args);
     assert(getRes.render.calledWithExactly('payment', sinon.match.object), '`res.render` is not being called with the correct arguments.');
 
     assert(typeof postHandleSpy === 'function', 'The payment post route may not exist.');
